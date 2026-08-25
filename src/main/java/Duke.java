771e0ba7b38a55130671e4e9a5591c8a2e2c847e
LUZIYUE <luziyue451@gmail.com>
2026-08-25 16:31:25 +0800

    added storage for tasks and list command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,17 +4,33 @@ public class Duke {
         System.out.println("Zsiggy here. Make it quick.");
         System.out.println("What mess do you need me to sort out today?");
         Scanner scanner = new Scanner(System.in);
+
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (true) {
             String input = scanner.nextLine();
 
-
             if (input.equals("bye")) {
                 break;
-            }
-            System.out.println(input);
 
+            } else if (input.equals("list")) {
+                System.out.println("Fine. Here's what you've dumped on me:");
+
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+
+                System.out.println("Yeah, yeah. I've got it:");
+                System.out.println(input);
+            }
         }
 
+        scanner.close();
         System.out.println("Hmph. Bye. Go drink your green milk tea.");
     }
 }
