ee24a19aa299074ecb7d29987dd077a4e6da0a50
LUZIYUE <luziyue451@gmail.com>
2026-08-25 16:23:40 +0800

    Add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Zsiggy here. Make it quick.");
         System.out.println("What mess do you need me to sort out today?");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+
+        }
+
         System.out.println("Hmph. Bye. Go drink your green milk tea.");
     }
 }
