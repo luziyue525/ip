592a623e2f9f10620088cd44a74ef63da9ab0bf8
LUZIYUE <luziyue451@gmail.com>
2026-08-27 23:42:04 +0800

    Handle task dates and times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,31 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private final String deadline;
+    private final LocalDate deadline;
 
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+
+        this.deadline = LocalDate.parse(
+                deadline,
+                DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        );
     }
 
     public String getDeadline() {
-        return this.deadline;
+        return this.deadline.format(
+                DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        );
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+        return "[D]" + super.toString()
+                + " (by: "
+                + this.deadline.format(
+                DateTimeFormatter.ofPattern("MMM d yyyy")
+        )
+                + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,25 +1,47 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private final String fromDate;
-    private final String toDate;
+    private final LocalDate fromDate;
+    private final LocalDate toDate;
 
     public Event(String description, String fromDate, String toDate) {
         super(description);
-        this.fromDate = fromDate;
-        this.toDate = toDate;
+
+        this.fromDate = LocalDate.parse(
+                fromDate,
+                DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        );
+
+        this.toDate = LocalDate.parse(
+                toDate,
+                DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        );
     }
 
     public String getFromDate() {
-        return this.fromDate;
+        return this.fromDate.format(
+                DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        );
     }
 
     public String getToDate() {
-        return this.toDate;
+        return this.toDate.format(
+                DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        );
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: "
-                + this.fromDate + " to: "
-                + this.toDate + ")";
+        return "[E]" + super.toString()
+                + " (from: "
+                + this.fromDate.format(
+                DateTimeFormatter.ofPattern("MMM d yyyy")
+        )
+                + " to: "
+                + this.toDate.format(
+                DateTimeFormatter.ofPattern("MMM d yyyy")
+        )
+                + ")";
     }
 }
